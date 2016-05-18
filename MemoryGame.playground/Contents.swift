5226c5b43faa7c9a5396d77e82361b832d4f9ccb
kh4zad <arcastro@gmail.com>
2016-05-18 11:06:58 +0200

    Playground finished

--- MemoryGame.playground/Contents.swift
+++ MemoryGame.playground/Contents.swift
@@ -2,4 +2,28 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var range = 0...100
+
+for number in range {
+    
+    // Número divisible entre 5
+    if number % 5 == 0 {
+        print("\(number) Bingo!!!")
+    }
+    
+    // Número par
+    if number % 2 == 0 {
+         print("\(number) par!!!")
+    }
+    
+    // Número impar
+    if number % 2 != 0 {
+        print("\(number) impar!!!")
+    }
+    
+    // Rango definido
+    if number >= 30 && number <= 40 {
+        print("\(number) Viva Swift!!!")
+    }
+    
+}
